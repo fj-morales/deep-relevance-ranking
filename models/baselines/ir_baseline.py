e6a0f8ef426f1c50fb8719c3b3e88211158c9655
fj-morales <fjmoralesmartinez@hotmail.com>
2019-07-10 00:30:26 +0200

    Indri baseline

--- models/baselines/ir_baseline.py
+++ models/baselines/ir_baseline.py
@@ -1,319 +1,190 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# ## Converting pkl files to json files to be read in galago tool
-
-# In[53]:
+# In[ ]:
 
 
-import pickle
-import json
-import gzip
-import os
+# import pickle
+# import json
+# import gzip
+# import os
 import subprocess
-import numpy as np
-import multiprocessing
-
-# In[78]:
-
-
-# Define paths
-#dataloc = '../../bioasq_data/'
-dataloc = '../../robust04_data/split_4/'
-baseline_files ='./baseline_files/'
-galago_loc='./galago-3.10-bin/bin/'
-
-
-# In[55]:
-
-
-def pickle_to_json(pickle_filename):
-    # Pickle to Trectext converter
-    doc_list = []
-    with open(dataloc + pickle_filename, 'rb') as f_in:
-        data = pickle.load(f_in)
-        if not os.path.exists(baseline_files):
-            os.makedirs(baseline_files)
-        out_file = baseline_files + pickle_filename[:-4] + '.gz'
-        with gzip.open(out_file,'wt', encoding='utf-8') as f_out:
-            docu = {}
-            for key, value in data.items():
-                if "pmid" in value.keys():
-                    doc_code = value.pop('pmid')
-                else:
-                    doc_code = key
-                f_out.write('<DOC>\n' + 
-                            '<DOCNO>' + doc_code + '</DOCNO>\n' +
-                            '<TITLE>' + value.pop('title') + '</TITLE>\n' +
-                            '<TEXT>' + value.pop('abstractText') + '</TEXT>\n' + 
-                            '</DOC>\n')
-                doc_list.append(doc_code)
-        return [out_file, doc_list]
-
-
-# In[56]:
-
-
-# Build corpus index 
-def build_index(index_input, index_loc):
-    index_input_param = '--inputPath+' + index_input    
-    index_loc_param = '--indexPath=' + index_loc
-    print(index_input_param)
-    print(index_loc_param)
-    if not os.path.exists(index_loc):
-            os.makedirs(index_loc) 
-    index_proc = subprocess.Popen(
-            [galago_loc + 'galago', 'build', '--stemmer+krovetz',
-                index_input_param, index_loc_param],
-            stdout=subprocess.PIPE, shell=False)
-    (out, err) = index_proc.communicate()
-    print(out.decode("utf-8"))
-    print(err)
-
-
-# In[57]:
-
-
-# # Return top 100 bm25 scored docs, given query and corpus indexed by galago
-# def get_bm25_docs(query, index_loc):
-#     index_loc_param = '--index=' + index_loc   
-#     if "'" in query:
-#         query_param = '--query="#stopword(' + query.rstrip('.') + ')"' 
-#     else:
-#         query_param = '--query=\'#stopword(' + query.rstrip('.') + ')\'' 
-        
-# #     print(query_param)
-
-#     command = galago_loc + 'galago batch-search --verbose=false --requested=100 ' + \
-#          index_loc_param + ' --scorer=bm25 --stemmer+krovetz ' + \
-#          query_param + ' | cut -d" " -f3'
-#     galago_bm25_exec = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-#     (out, err) = galago_bm25_exec.communicate()
-#     bm25_documents = out.decode("utf-8")
-#     return bm25_documents.splitlines()
-
-
-# In[75]:
-
-
-# Return top 100 bm25 scored docs, given query and corpus indexed by galago
-def get_bm25_docs(query, index_loc, b_val, k_val):
-    query = query.lower()
-    index_loc_param = '--index=' + index_loc  
-    b=' --b=' + str(b_val)
-    k=' --k=' + str(k_val)
-    if "'" in query:
-        query_param = '--query="#stopword(' + query.rstrip('.') + ')"' 
-    else:
-        query_param = '--query=\'#stopword(' + query.rstrip('.') + ')\'' 
-
-    command = galago_loc + 'galago batch-search --verbose=false --requested=100 ' +          index_loc_param + ' --scorer=bm25' +          b +          k +          ' --stemmer+krovetz ' +          query_param + ' | cut -d" " -f3'
-#     print(command)
-    galago_bm25_exec = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    (out, err) = galago_bm25_exec.communicate()
-    bm25_documents = out.decode("utf-8")
-    return bm25_documents.splitlines()
-
-
-# In[59]:
-
-
-# # ## Testing (remove)
-
 # import numpy as np
-# index_loc = '/home/fmorales/msc_project/not-a-punching-bag/reproduction/deep-relevance-ranking/models/baselines/baseline_files/index_bioasq_test'
-# # # q = 'Has \"RNA interference\" been awarded Nobel prize.'
-# # # q = 'Describe Wellens\' Syndrome.'
-# # # q = 'Can the Micro-C XL method achieve mononucleosome resolution?'
-# # # q = 'What is the role of the UBC9 enzyme in the protein sumoylation pathway?'
-# # # q = "Has \"RNA interference\" been awarded Nobel prize?"
-# q = "What is the role of gamma-secreatase complex in Alzheimer's Disease?"
-# # q = "List diseases associated with the  Dopamine Receptor D4 (DRD4)."
-# # "List the classical symptoms of the Moschcowitz syndrome (Thrombotic thrombocytopenic purpura)."
-# # q = "List the classical symptoms of the Moschcowitz syndrome (Thrombotic thrombocytopenic purpura)."
+# import multiprocessing
+# import re 
+# import csv
+# import torch
+# import sys
+# import shutil
+# import random
 
-# b_range = np.arange(0.4, 0.9, 0.1)
-# k_range = np.arange(1,2.5,0.2)
-# for b in b_range:
-#     for k in k_range:
-#         print(round(b,1),round(k,1))
-#         valor = get_bm25_docs(q, index_loc, round(b,1), round(k,1))
-#         print(valor)
+# import argparse
 
+# import uuid
+# import datetime
+# import time
 
-# In[60]:
+# import bz2
+# import pandas as pd
+# # import dbmanager  as dbmanager
+# from os.path import join
 
+## My libraries
 
-pkl_files = [ x for x in os.listdir(dataloc) if all(y in x for y in ['docset', '.pkl'])]
-
-
-# In[61]:
-
-
-pkl_files
-
-
-# In[62]:
-
-
-# Convert pickle to trectext file formar to be processed with galago
-for pkl_file in pkl_files[0:1]:
-    [output_file, doc_list ]= pickle_to_json(pkl_file)
-
-
-# In[63]:
-
-
-if "dev" in output_file:
-    print("dev")
-    data_split = "dev"
-elif "test" in output_file:
-    print("test")
-    data_split = "test"
-elif "train" in output_file:
-    print("train")
-    data_split = train
-
-    
-if "rob04" in output_file:
-    print("rob04")
-    dataset_name = "rob04"
-elif "bioasq" in output_file:
-    print("bioasq")
-    dataset_name = "bioasq"
-
-
-# In[64]:
-
-
-index_loc = baseline_files + 'index' + '_' + dataset_name + '_' + data_split
-index_input = output_file
-# build_index(index_input, index_loc)
-
-
-# In[65]:
-
-
-q_filename = [ x for x in os.listdir(dataloc) if all(y in x for y in [dataset_name +'.'+ data_split, '.json'])]
-
-
-# In[66]:
-
-
-q_filename
-
-
-# In[67]:
-
-
-queries_file = dataloc + q_filename[0]
-with open(queries_file, 'rb') as input_file:
-    query_data = json.load(input_file)
-
-
-# In[68]:
-
-
-def save_preds(file, preds):
-    with open(file, 'wt') as f_out:
-        json.dump(preds, f_out, indent=4)
-    print('Predictions file: ' + file + ', done!')
-
-
-# In[69]:
-
-
-print(index_loc)
+import utils
+import bioasq_corpus_parser
+import bioasq_query_parser
+# from utils import *
 
 
 # In[ ]:
 
-def bm25_computing(b_k):
-    b = b_k[0]
-    k = b_k[1]
-    bm25_preds_file = baseline_files + 'bm25_preds_' + dataset_name + '_'+ data_split + '_' + 'b' + str(b) + 'k' + str(k) + '.json'
-    print(bm25_preds_file)
-    if os.path.isfile(bm25_preds_file):
-        print(bm25_preds_file + "...done!")
-        return
-    bm25_preds = {}
-    questions = []
-    question = {}
-    for query in query_data['questions']:
-        question['body'] = query['body']
-        question['id'] = query['id']
-    #     print(query['body'].rstrip('.'))
-    #     documents = get_bm25_docs(query['body'].rstrip('.'), index_loc)
-        documents = get_bm25_docs(query['body'], index_loc, b, k)
-        if "bioasq" in dataset_name: 
-            documents_url = ['http://www.ncbi.nlm.nih.gov/pubmed/' + doc for doc in documents]
-            question['documents'] = documents_url
-        elif "rob04" in dataset_name:
-            question['documents'] = documents
-        questions.append(dict(question))
+
+class Index:
+    def __init__(self, ir_toolkit_location, parameter_file_location):
+        self.ir_toolkit_location = ir_toolkit_location
+        self.parameter_file_location = parameter_file_location
+#     def build(self, ir_tool_params):
+    def build(self):
+        
+#         utils.create_dir(self.index_location)
+    #     index_loc_param = '--indexPath=' + index_loc
+        stopwords_file = './stopwords'
+        build_index_command = self.ir_toolkit_location + 'bin/IndriBuildIndex'
+        toolkit_parameters = [
+                                build_index_command,
+                                self.parameter_file_location,
+                                stopwords_file
+                                ]
+
+        print(toolkit_parameters)
+
+        proc = subprocess.Popen(toolkit_parameters,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+        (out, err) = proc.communicate()
+        print(out.decode("utf-8"))
+        print('Index error: ', err)
+        if err == None:
+            return 'Ok'
+
+
+# In[ ]:
+
+
+class Query:
+    def __init__(self, ir_toolkit_location, query_file, query_parameter_file, run_filename):
+        self.ir_toolkit_location = ir_toolkit_location
+        self.query_file = query_file
+        self.query_parameter_file = query_parameter_file
+        self.run_filename = run_filename
+        
+#     def build(self, ir_tool_params):
+    def run(self):
+        
+#         utils.create_dir(self.index_location)
     
-    bm25_preds['questions'] = questions
-    save_preds(bm25_preds_file, bm25_preds)    
+        stopwords_file = './stopwords'
+        query_command = self.ir_toolkit_location + 'bin/IndriRunQuery'
+        toolkit_parameters = [
+                                query_command,
+                                self.query_file,
+                                self.query_parameter_file,
+                                stopwords_file,
+                                '>',
+                                self.run_filename]
 
-def start_process():
-    print( 'Starting', multiprocessing.current_process().name)
+        print(toolkit_parameters)
 
-# Multiprocessing for testing different bb and k
-
-if __name__ == '__main__':
-    defaults = 'no'
-    if defaults == 'yes':
-        brange = [0.75]
-        krange = [1.2]
-    else:
-        brange = np.arange(0.2,1,0.1)
-        krange = np.arange(0.5,2,0.1)
-    b_k = [(round(b,2), round(k,2)) for b in brange for k in krange]
-    print(b_k[0][0])
-
-    pool_size = 8
-    pool = multiprocessing.Pool(processes=pool_size,
-                                initializer=start_process,
-                                )
-    pool_outputs = pool.map(bm25_computing, b_k)
-    pool.close() # no more tasks
-    pool.join()  # wrap up current tasks
-
-# ## Experiments (remove safely)
-
-# In[71]:
+        proc = subprocess.Popen(toolkit_parameters,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+        (out, err) = proc.communicate()
+        print(out.decode("utf-8"))
+        print('Run error: ', err)
+        if err == None:
+            return 'Ok'
 
 
-# from random import sample
-# random_preds = {}
-# questions = []
-# question = {}
-# for query in query_data['questions']:
-#     question['body'] = query['body']
-#     question['id'] = query['id']
-# #     print(query['body'].rstrip('.'))
-# #     documents = get_bm25_docs(query['body'].rstrip('.'), index_loc)
-#     documents = sample(doc_list, 100)
-#     if "bioasq" in dataset_name: 
-#         documents_url = ['http://www.ncbi.nlm.nih.gov/pubmed/' + doc for doc in documents]
-#         question['documents'] = documents_url
-#     elif "rob04" in dataset_name:
-#         question['documents'] = documents
-#     questions.append(dict(question))
+# In[80]:
+
+
+def eval(trec_eval_command, qrel, qret):
     
-# random_preds['questions'] = questions
+    metrics = '-m map -m P.20 -m ndcg_cut.20'
+    toolkit_parameters = [
+                            trec_eval_command,
+                            '-m',
+                            'map',
+                            '-m',
+                            'P.20',
+                            '-m',
+                            'ndcg_cut.20',
+                            qrel,
+                            qret]
+
+    print(toolkit_parameters)
+
+    proc = subprocess.Popen(toolkit_parameters, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
+    (out, error) = proc.communicate()
+    print(out.decode("utf-8"))
+    print('Run error: ', err)
+    if err == None:
+        return 'Ok'
 
 
-# In[72]:
+# In[ ]:
 
 
-# random_preds_file = baseline_files + 'random_preds.' + dataset_name + data_split + '.json'
-# with open(random_preds_file, 'wt') as f_out:
-#     json.dump(random_preds, f_out, indent=4)
+if __name__ == "__main__":
+    
+#     ir_toolkit_location = sys.argv[1] # '../indri/'
+#     parameter_file_location = sys.argv[2] # './bioasq_index_param_file'
 
-
-# In[73]:
-
-
-# len(doc_list)
+    # create dataset files dir
+    
+    dataset = 'bioasq'
+    workdir = dataset + '_dir/'
+    
+    # generate corpus files to index
+    
+    pool_size = 40 # scales very well when server is not used
+    # Get all filenames
+    data_dir = '/ssd/francisco/pubmed19/'
+    to_index_dir =  workdir + dataset + '_corpus/'
+    index_dir = workdir + dataset + '_indri_index'
+    utils.create_dir(to_index_dir)
+    utils.create_dir(index_dir)
+    bioasq_corpus_parser.corpus_parser(data_dir, to_index_dir, pool_size)
+    
+    
+    # Generate query files
+    
+    
+    ir_toolkit_location = '../../../indri/'
+    trec_eval_command = '../../eval/trec_eval'
+    parameter_file_location = workdir + 'bioasq_index_param_file'
+    
+    index_data = Index(ir_toolkit_location, parameter_file_location)
+    index_data.build()
+    
+    # Generate qrels and qret
+    
+    queries_file = '../../bioasq_data/bioasq.test.json'
+    
+    prefix = queries_file.split('/')[-1].strip('.json')
+    filename_prefix = workdir + prefix
+    
+    print(filename_prefix)
+    
+    trec_query_file = filename_prefix + '_trec_query'
+    qrels_file = filename_prefix + '_qrels'
+    
+    bioasq_query_parser.query_parser(queries_file, trec_query_file, qrels_file)
+    
+    # Run query
+    
+    run_filename = workdir + 'run_' + prefix
+    query_parameter_file = workdir + dataset + '_query_params'
+    bm25_query = Query(ir_toolkit_location, trec_query_file, query_parameter_file, run_filename)
+    bm25_query.run()
+    
+    # Eval
+    eval(trec_eval_command, qrels_file, run_filename)
 
