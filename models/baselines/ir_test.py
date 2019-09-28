043b14de4dc1e8318604eca416e275c9a1744f94
fj-morales <fjmoralesmartinez@hotmail.com>
2019-09-28 21:23:32 +0200

    Fixed minor bugs with robust dataset

--- models/baselines/ir_test.py
+++ models/baselines/ir_test.py
@@ -7,7 +7,7 @@ def generate_run_file(pre_run_file, run_file):
     
     with open(pre_run_file, 'rt') as input_f:
         pre_run = input_f.readlines()
-#         print(type(pre_run))
+        print(type(pre_run))
     with open(run_file, 'wt') as out_f:
         for line in pre_run:
             out_f.write(line.replace('docid=','').replace('indri', 'lambdaMART'))
@@ -15,6 +15,8 @@ def generate_run_file(pre_run_file, run_file):
 
 def gen_run_file(ranklib_location, normalization, save_model_file, test_data_file, run_file):
 # Works also for testing
+
+    print("Run file: ",run_file)
     ranker_command = ['java', '-jar', ranklib_location + 'RankLib-2.12.jar']
     pre_run_file = run_file.replace('run_', 'pre_run_', 1)
     toolkit_parameters = [
@@ -28,6 +30,7 @@ def gen_run_file(ranklib_location, normalization, save_model_file, test_data_fil
         pre_run_file     
         ]             
 
+#     print(toolkit_parameters)
     proc = subprocess.Popen(toolkit_parameters,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
     (out, err)= proc.communicate()
     #         print(out.decode('utf-8').splitlines())
@@ -54,18 +57,18 @@ def test_model(workdir, dataset, ranklib_location, trec_eval_command, normalizat
             dataset_fold = dataset 
             test_data_file = fold_dir + dataset + '_test' +  '_features'
             qrels_test_file = fold_dir + dataset + '_test' + '_qrels'
+            test_data_file = fold_dir + dataset_fold + '_test' + '_features'
         else:
             fold_dir = workdir + 's' + fold + '/'
             dataset_fold = dataset + '_s' + fold
             test_data_file = fold_dir + dataset + '_test' + '_s' + fold +  '_features'
             qrels_test_file = fold_dir + dataset + '_test' + '_s' + fold + '_qrels'
+            test_data_file = fold_dir + dataset + '_test' + '_s' + fold  + '_features'
 
 
         ## Evaluate on test
 
         run_test_file = fold_dir + 'run_' + dataset_fold + '_best_lmart_test' 
-        
-        test_data_file = fold_dir + dataset + '_test' + '_features'
         
         config_results = res.get_runs_by_id(res.get_incumbent_id())
         
